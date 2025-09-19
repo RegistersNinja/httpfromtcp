e3a3012a2b07b4d47ca2686444520f17950454ff
RegistersNinja <RegistersNinja@RegistersNinja.com>
2025-09-19 14:48:40 +0300

    Newlines

--- main.go
+++ main.go
@@ -1,44 +1,60 @@
 package main
 
-
 import (
 	"fmt"
+	"io"
 	"os"
+	"strings"
 )
 
-
-const bytesToRead int = 8
-
+const (
+	bytesToRead int = 8
+	emptyRead   int = 0
+	ExitError   int = 1
+	readNewLine int = 2
+)
 
 func main() {
 	var (
-		messages *os.File
-		err error
-		bytesRead int
-		buffer []byte
+		messages     *os.File
+		err          error
+		bytesRead    int
+		buffer       []byte
+		line         string
+		newLineSplit []string
 	)
 	buffer = make([]byte, bytesToRead)
-
 
 	messages, err = os.Open("messages.txt")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "failed to open file: %v\n", err)
-		os.Exit(1)
+		os.Exit(ExitError)
 	}
-
 
 	for {
 		bytesRead, err = messages.Read(buffer)
 		if err != nil {
-			if err.Error() != "EOF" {
+			if err != io.EOF {
 				fmt.Fprintf(os.Stderr, "failed to read from file: %v\n", err)
-				os.Exit(1)
+				os.Exit(ExitError)
 			}
 			break
 		}
-		if bytesRead > 0 {
-			fmt.Printf("read: %s\n", string(buffer))
+		if bytesRead > emptyRead {
+			line += string(buffer[:bytesRead])
+		}
+
+		newLineSplit = strings.Split(line, "\n")
+		if len(newLineSplit) == readNewLine {
+			fmt.Printf("read: %s\n", newLineSplit[0])
+			
+			line = newLineSplit[1]
+			clear(newLineSplit)
 		}
 	}
-	
+
+	if len(line) > 0 {
+        fmt.Printf("read: %s\n", line)
+    }
+
 }
