e320a28ee6275436ca6e747c13995703bd227317
RegistersNinja <RegistersNinja@RegistersNinja.com>
2025-09-19 14:48:40 +0300

    8 Bytes

--- main.go
+++ main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+
+import (
+	"fmt"
+	"os"
+)
+
+
+const bytesToRead int = 8
+
 
 func main() {
-	fmt.Print("I hope I get the job!\n")
+	var (
+		messages *os.File
+		err error
+		bytesRead int
+		buffer []byte
+	)
+	buffer = make([]byte, bytesToRead)
+
+
+	messages, err = os.Open("messages.txt")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to open file: %v\n", err)
+		os.Exit(1)
+	}
+
+
+	bytesRead, err = messages.Read(buffer)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to read from file: %v\n", err)
+		os.Exit(1)
+	}
+
+	for(bytesRead == 8) {
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "failed to read from file: %v\n", err)
+			os.Exit(1)
+		}
+		
+		fmt.Printf("read: %s\n", string(buffer))
+		clear(buffer)
+		bytesRead, err = messages.Read(buffer)
+	}
+	
+	if err == nil {
+		fmt.Printf("read: %s\n", string(buffer))
+	} else {
+			fmt.Fprintf(os.Stderr, "failed to read from file: %v\n", err)
+			os.Exit(1)
+	}
 }
