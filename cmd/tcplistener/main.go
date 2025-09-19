e218ee0e2f5d36f07626ba97edb8ec67f51a566c
RegistersNinja <RegistersNinja@RegistersNinja.com>
2025-09-19 14:48:40 +0300

    Connect the Parsing

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -2,67 +2,22 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"net"
 	"os"
-	"strings"
+
+	"github.com/RegistersNinja/httpfromtcp/internal/request"
 )
 
 const (
-	bytesToRead int = 8
-	emptyRead   int = 0
-	ExitError   int = 1
-	channelSize int = 1
-	localAddr string = "127.0.0.1:42069"
+	ExitError   int    = 1
+	localAddr   string = "127.0.0.1:42069"
 )
-
-func getLinesChannel(conn net.Conn) <-chan string {
-	out := make(chan string, channelSize)
-
-	go func() {
-		defer conn.Close()
-		defer close(out)
-
-		var (
-			buffer       []byte
-			line         string
-			newLineSplit []string
-		)
-
-		buffer = make([]byte, bytesToRead)
-		for {
-			bytesRead, err := conn.Read(buffer)
-			if err != nil {
-				if err != io.EOF {
-					fmt.Fprintf(os.Stderr, "failed to read from file: %v\n", err)
-				}
-				break
-			}
-			if bytesRead > emptyRead {
-				line += string(buffer[:bytesRead])
-			}
-
-			newLineSplit = strings.Split(line, "\n")
-			for i := 0; i < len(newLineSplit)-1; i++ {
-				out <- newLineSplit[i]
-			}
-			line = newLineSplit[len(newLineSplit)-1]
-		}
-
-		if len(line) > 0 {
-			out <- line
-		}
-
-	}()
-
-	return out
-}
 
 func main() {
 	var (
 		listener net.Listener
 		conn     net.Conn
-		line     string
+		r        *request.Request
 		err      error
 	)
 
@@ -81,13 +36,17 @@ func main() {
 			os.Exit(ExitError)
 		}
 
-		if conn != nil {
-			fmt.Print("A connection has been accepted\n")
-			for line = range getLinesChannel(conn) {
-				fmt.Print(line)
-			}
-			fmt.Print("\nThe connection has been closed.\n")
+		r, err = request.RequestFromReader(conn)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "failed to parse the request: %v\n", err)
+			os.Exit(ExitError)
 		}
+
+		fmt.Printf("Request line:\n")
+		fmt.Printf("- Method: %s\n", r.RequestLine.Method)
+		fmt.Printf("- Target: %s\n", r.RequestLine.RequestTarget)
+		fmt.Printf("- Version: %s\n", r.RequestLine.HttpVersion)
+
 	}
 
 }
