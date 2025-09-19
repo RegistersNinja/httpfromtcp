a1b4a92438891c155196aa06f487a0b3b963ed2e
RegistersNinja <RegistersNinja@RegistersNinja.com>
2025-09-19 14:48:40 +0300

    Channel Refactor

--- main.go
+++ main.go
@@ -11,19 +11,55 @@ const (
 	bytesToRead int = 8
 	emptyRead   int = 0
 	ExitError   int = 1
-	readNewLine int = 2
 )
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	out := make(chan string, 1)
+
+	go func() {
+		defer f.Close()
+		defer close(out)
+		
+		var (
+			buffer       []byte
+			line         string
+			newLineSplit []string
+		)
+
+		buffer = make([]byte, bytesToRead)
+		for {
+			bytesRead, err := f.Read(buffer)
+			if err != nil {
+				if err != io.EOF {
+					fmt.Fprintf(os.Stderr, "failed to read from file: %v\n", err)
+				}
+				break
+			}
+			if bytesRead > emptyRead {
+				line += string(buffer[:bytesRead])
+			}
+
+			newLineSplit = strings.Split(line, "\n")
+			for i := 0; i < len(newLineSplit)-1; i++ {
+				out <- newLineSplit[i]
+			}
+			line = newLineSplit[len(newLineSplit)-1]
+		}
+
+		if len(line) > 0 {
+			out <- line
+		}
+
+	}()
+
+	return out
+}
 
 func main() {
 	var (
-		messages     *os.File
-		err          error
-		bytesRead    int
-		buffer       []byte
-		line         string
-		newLineSplit []string
+		messages *os.File
+		err      error
 	)
-	buffer = make([]byte, bytesToRead)
 
 	messages, err = os.Open("messages.txt")
 	if err != nil {
@@ -31,30 +67,8 @@ func main() {
 		os.Exit(ExitError)
 	}
 
-	for {
-		bytesRead, err = messages.Read(buffer)
-		if err != nil {
-			if err != io.EOF {
-				fmt.Fprintf(os.Stderr, "failed to read from file: %v\n", err)
-				os.Exit(ExitError)
-			}
-			break
-		}
-		if bytesRead > emptyRead {
-			line += string(buffer[:bytesRead])
-		}
-
-		newLineSplit = strings.Split(line, "\n")
-		if len(newLineSplit) == readNewLine {
-			fmt.Printf("read: %s\n", newLineSplit[0])
-			
-			line = newLineSplit[1]
-			clear(newLineSplit)
-		}
+	for line := range getLinesChannel(messages) {
+		fmt.Printf("read: %s\n", line)
 	}
-
-	if len(line) > 0 {
-        fmt.Printf("read: %s\n", line)
-    }
 
 }
